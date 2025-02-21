8ff1b6c5cdcbdce0efd231f870f1b57f422754db
Fxdij <fadiasadj@gmail.com>
2025-02-20 21:35:35 -0500

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,27 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "FHA SOLUTIONS INC.",
   description: "FHA SOLUTIONS INC. - Innovative Solutions for Modern Challenges",
+  icons: {
+    icon: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/favicon-96x96-o3txOS8S99UCLIByNIrZNcCPwWL0iZ.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/apple-touch-icon-vYCeX8z441vcqU46jnWRcjv1FvXzuo.png",
+      },
+    ],
+    other: [
+      {
+        rel: "manifest",
+        url: "/manifest.json",
+      },
+    ],
+  },
+  manifest: "/manifest.json",
     generator: 'v0.dev'
 }
 
